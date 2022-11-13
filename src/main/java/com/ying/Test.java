31341008fd7d7b08a6d0f646a983991ca4f73380
ying <1372831277@qq.com>
2022-11-13 16:24:43 +0800

    first commit

---
+++ src/main/java/com/ying/Test.java
@@ -0,0 +1,14 @@
+package com.ying;
+
+/**
+ * @author ying
+ * @create 2022-11-13-16:18
+ */
+public class Test
+{
+
+    public static void main(String[] args)
+    {
+        System.out.println("hello git");
+    }
+}
